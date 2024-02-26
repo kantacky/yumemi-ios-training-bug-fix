e4e25d1d5b235e01b130368b1f3246a3fb321365
Kanta Oikawa <k_oikawa@yumemi.co.jp>
2024-02-26 18:56:31 +0900

    Update to call completion handler when failed to encode json

--- Example/Example/Model/WeatherModel.swift
+++ Example/Example/Model/WeatherModel.swift
@@ -52,6 +52,8 @@ class WeatherModelImpl: WeatherModel {
                     }
                 }
             }
+        } else {
+            completion(.failure(WeatherError.jsonEncodeError))
         }
     }
 }
